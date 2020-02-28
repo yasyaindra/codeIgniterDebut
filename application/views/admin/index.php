af16315a1121425ca46d81207c349f4b2da15087
yasyaindra <yasyaindra@github.com>
2020-02-28 15:12:53 +0700

    menambahkan user access management

---
+++ application/views/admin/index.php
@@ -0,0 +1,20 @@
+        <!-- Sidebar -->
+
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+        </div>
+        <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -15,12 +15,15 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
                                     </div>
-                                    <form class="user">
+                                    <?= $this->session->flashdata('message'); ?>
+                                    <form class="user" action="<?= base_url('index.php') ?>" method="post">
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" id="exampleInputEmail" placeholder="Enter Email Address..." id="email" name="email">
+                                            <input type="text" class="form-control form-control-user" placeholder="Enter Email Address..." id="email" name="email" value="<?= set_value('email') ?>">
+                                            <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password" placeholder="Password">
+                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                                            <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Login
